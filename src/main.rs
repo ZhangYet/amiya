033350b90306d1f5cecd8808388c2ec4b5a673ef
dantezy <zhangyet@gmail.com>
2019-07-01 16:32:46 +0800

    We got an echo command line app now.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Doctor!");
+    loop {
+        print!("Doctor: ");
+        let _ = io::stdout().flush();
+        let mut s = String::new();
+        io::stdin().read_line(&mut s).expect("Did not enter a correct string");
+        print!("Amiya: {}", s);
+    }
 }
